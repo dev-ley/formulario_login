b26840a61e6a72faf464612d67766aafe642d6b7
Wesley Rodrigues <122097397+dev-ley@users.noreply.github.com>
2024-04-29 20:22:28 -0300

    removendo link do registro

--- src/routes/Registro.jsx
+++ src/routes/Registro.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useState } from "react";
 import styled from 'styled-components'
 import { IoMdPerson, IoIosUnlock } from "react-icons/io";
--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,6 +1,3 @@
-import { Link } from 'react-router-dom'
-
-
 import React from 'react'
 import styled from 'styled-components'
 
